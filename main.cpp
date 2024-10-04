a255922771b157715d8c2042d0fe0e46acd6ae91
Filippo Dalmonego <filippodalm@gmail.com>
2024-10-04 11:57:33 +0200

    aggiunta catch

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <random>
 #include <sstream>
+#include <stdexcept>
 #include <vector>
-
 // Funzione per calcolare la distanza media tra tutte le coppie di boids
 double meanDistance(Flock const& flock)
 {
@@ -62,141 +62,149 @@ double stdDevDistance(Flock const& flock, double mean)
 
 int main()
 {
-  // display
-  auto const display_width  = sf::VideoMode::getDesktopMode().width;
-  auto const display_height = sf::VideoMode::getDesktopMode().height;
+  try {
+    // display
+    auto const display_width  = sf::VideoMode::getDesktopMode().width;
+    auto const display_height = sf::VideoMode::getDesktopMode().height;
 
-  std::cout << "Display width = " << display_width << " ; "
-            << "Display height : " << display_height << '\n';
+    std::cout << "Display width = " << display_width << " ; "
+              << "Display height : " << display_height << '\n';
 
-  // variabili
-  auto const delta_t{sf::milliseconds(33)};
+    // variabili
+    auto const delta_t{sf::milliseconds(33)};
 
-  // creazione di  "Flock f"
-  unsigned int numBoids;
-  double a;
-  double c;
-  double s;
-  char bruh;
+    // creazione di  "Flock f"
+    unsigned int numBoids;
+    double a;
+    double c;
+    double s;
+    char bruh;
 
-  std::cout << "inserire il numero dei boids: " << '\n';
-  std::cin >> numBoids;
+    std::cout << "inserire il numero dei boids: " << '\n';
+    std::cin >> numBoids;
 
-  std::cout << "cambiare i coefficienti? (Y : yes   N : no)" << '\n';
-  std::cin >> bruh;
-  if (bruh == 'Y') {
-    std::cout << "inserire i coefficienti: " << '\n';
-    std::cin >> a >> c >> s;
-  } else {
-    a = 1.7;
-    c = 3.;
-    s = 6.;
-  }
-
-  // creazione flock
-  std::vector<Boid> flock(numBoids);
-  // inizializzazione immediata visto che conosco già grandezza,
-  // evita riallocazioni multiple della memoria man mano che
-  // riempio
-
-  std::random_device r;
-  std::default_random_engine gen{r()};
-
-  // riempimento random
-  for (unsigned int i = 0; i != numBoids; i++) {
-    std::uniform_real_distribution<double> random_height(200.,
-                                                         display_height - 300.);
-    std::uniform_real_distribution<double> random_width(200.,
-                                                        display_width - 300.);
-    std::uniform_real_distribution<double> random_velocity(-50., 1.);
-
-    flock[i].pos.setX(random_width(gen));
-    flock[i].pos.setY(random_height(gen));
-    flock[i].vel.setX(random_velocity(gen));
-    flock[i].vel.setY(random_velocity(gen));
-
-    flock[i].N = i;
-  }
-
-  Flock f{flock, a, c, s};
-
-  // creazione finestra sfml
-  sf::RenderWindow window(sf::VideoMode(display_width, display_height),
-                          "Boids simulation");
-  window.setVerticalSyncEnabled(true);
-
-  // caricamento immagine e testi
-  sf::Font font;
-  if (!font.loadFromFile("arial.ttf")) {
-    // Gestisci l'errore se il font non viene caricato
-    return -1;
-  }
-
-  sf::Text statsText;
-  statsText.setFont(font);
-  statsText.setCharacterSize(24);
-  statsText.setFillColor(sf::Color::Black);
-  statsText.setPosition(10.f, 10.f); // Posizione del testo nella finestra
-
-  sf::Texture texture;
-  if (!texture.loadFromFile("../images/Boid.png")) {
-    throw std::runtime_error(
-        "Impossibile caricare la texture '../images/Boid.png'");
-  }
-
-  sf::Sprite sprite;
-  sprite.setTexture(texture);
-  sprite.setScale(0.019f, 0.019f);
-
-  sf::Texture texture1;
-  if (!texture1.loadFromFile("../images/cielo.jpg")) {
-    std::cout << "Could not load texture" << std::endl;
-    window.close();
-    return 0;
-  }
-  sf::Sprite sprite1;
-  sprite1.setTexture(texture1);
-  sprite1.setScale(1.17f, 1.17f);
-
-  bool wait = false;
-
-  // ciclo per stampa continua dei frame
-
-  window.setFramerateLimit(30);
-  while (window.isOpen()) {
-    sf::Event event;
-
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-    if (wait == true) { // codice quando il gioco è in pausa
+    std::cout << "cambiare i coefficienti? (Y : yes   N : no)" << '\n';
+    std::cin >> bruh;
+    if (bruh == 'Y') {
+      std::cout << "inserire i coefficienti: " << '\n';
+      std::cin >> a >> c >> s;
+    } else {
+      a = 1.7;
+      c = 3.;
+      s = 6.;
     }
 
-    if (wait == false) {
-      window.clear(sf::Color::White);
-      window.draw(sprite1);
+    // creazione flock
+    std::vector<Boid> flock(numBoids);
+    // inizializzazione immediata visto che conosco già grandezza,
+    // evita riallocazioni multiple della memoria man mano che
+    // riempio
 
-      double const dt{delta_t.asSeconds()};
-      f.evolve(dt, display_width, display_height);
+    std::random_device r;
+    std::default_random_engine gen{r()};
 
-      for (auto& u : f.flock_) {
-        sprite.setPosition(static_cast<float>(u.pos.getX()),
-                           static_cast<float>(u.pos.getY()));
-        double angle = std::atan2(u.vel.getY(), u.vel.getX()) * 180 / M_PI;
-        sprite.setRotation(static_cast<float>(angle + 90.));
-        window.draw(sprite);
+    // riempimento random
+    for (unsigned int i = 0; i != numBoids; i++) {
+      std::uniform_real_distribution<double> random_height(200., display_height
+                                                                     - 300.);
+      std::uniform_real_distribution<double> random_width(200.,
+                                                          display_width - 300.);
+      std::uniform_real_distribution<double> random_velocity(-50., 1.);
+
+      flock[i].pos.setX(random_width(gen));
+      flock[i].pos.setY(random_height(gen));
+      flock[i].vel.setX(random_velocity(gen));
+      flock[i].vel.setY(random_velocity(gen));
+
+      flock[i].N = i;
+    }
+
+    Flock f{flock, a, c, s};
+
+    // creazione finestra sfml
+    sf::RenderWindow window(sf::VideoMode(display_width, display_height),
+                            "Boids simulation");
+    window.setVerticalSyncEnabled(true);
+
+    // caricamento immagine e testi
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+      // Gestisci l'errore se il font non viene caricato
+      return -1;
+    }
+
+    sf::Text statsText;
+    statsText.setFont(font);
+    statsText.setCharacterSize(24);
+    statsText.setFillColor(sf::Color::Black);
+    statsText.setPosition(10.f, 10.f); // Posizione del testo nella finestra
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("../images/Boid.png")) {
+      throw std::runtime_error(
+          "Impossibile caricare la texture '../images/Boid.png'");
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setScale(0.019f, 0.019f);
+
+    sf::Texture texture1;
+    if (!texture1.loadFromFile("../images/cielo.jpg")) {
+      throw std::runtime_error(
+          "Impossibile caricare la texture '../images/Boid.png'");
+      /*std::cout << "Could not load texture" << std::endl;
+      window.close();
+      return 0;*/ //bisogna lasciare anche queste? Bisogna fare una funzione catch?
+    }
+    sf::Sprite sprite1;
+    sprite1.setTexture(texture1);
+    sprite1.setScale(1.17f, 1.17f);
+
+    bool wait = false;
+
+    // ciclo per stampa continua dei frame
+
+    window.setFramerateLimit(30);
+    while (window.isOpen()) {
+      sf::Event event;
+
+      while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+          window.close();
+      }
+      if (wait == true) { // codice quando il gioco è in pausa
       }
 
-      double mean    = meanDistance(f);
-      double std_dev = stdDevDistance(f, mean);
+      if (wait == false) {
+        window.clear(sf::Color::White);
+        window.draw(sprite1);
 
-      std::stringstream ss;
-      ss << "Distanza media: " << mean << "\nDeviazione standard: " << std_dev;
-      statsText.setString(ss.str());
+        double const dt{delta_t.asSeconds()};
+        f.evolve(dt, display_width, display_height);
 
-      window.draw(statsText);
-      window.display();
+        for (auto& u : f.flock_) {
+          sprite.setPosition(static_cast<float>(u.pos.getX()),
+                             static_cast<float>(u.pos.getY()));
+          double angle = std::atan2(u.vel.getY(), u.vel.getX()) * 180 / M_PI;
+          sprite.setRotation(static_cast<float>(angle + 90.));
+          window.draw(sprite);
+        }
+
+        double mean    = meanDistance(f);
+        double std_dev = stdDevDistance(f, mean);
+
+        std::stringstream ss;
+        ss << "Distanza media: " << mean
+           << "\nDeviazione standard: " << std_dev;
+        statsText.setString(ss.str());
+
+        window.draw(statsText);
+        window.display();
+      }
     }
+  } catch (const std::exception& e) {
+    std::cerr << "Errore irreversibile: " << e.what() << std::endl;
+    return EXIT_FAILURE; //posizione giusta di catch?
   }
 }
